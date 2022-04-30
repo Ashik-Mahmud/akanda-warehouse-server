62b0e3e0e5625a19dbb75e4b99dc6e72f58be7fc
ashikmahmud <ashikmamud187@gmail.com>
2022-04-30 07:46:24 +0600

    create our first and test api

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 /* init nodejs api */
+const express = require('express');
+const cors = require('cors');
+require("dotenv").config();
+// init 
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+/* middle wares */
+app.use(cors());
+app.use(express.json())
+
+/* fake root api endpoint */
+app.get("/", (req, res) =>{
+    res.send({message: "Now Our Akanda Warehouse API working."})
+})
+
+
+
+/* listen port */
+app.listen(port, ()=>{
+    console.log(`SERVER RUNNING ON PORT ${port}`);
+})
