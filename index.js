a1c4d643f82ef32912c7525821ca70cdf1e0e534
ashikmahmud <ashikmamud187@gmail.com>
2022-04-30 08:10:20 +0600

    yeah i fetch reviews data from mongodb and wow it's working

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* init nodejs api */
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config();
 // init 
 const app = express();
@@ -15,6 +16,39 @@ app.use(express.json())
 app.get("/", (req, res) =>{
     res.send({message: "Now Our Akanda Warehouse API working."})
 })
+
+
+/* run function for connect and do something with mongodb */
+
+const uri = `mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASSWORD}@akanda-warehouse.hanwa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+    try{
+     await client.connect();
+     console.log("DB CONNECTED.");
+     const reviewsCollection =  client.db("reviews-db").collection("reviews");
+     /* GET REVIEWS FROM MONGODB DATABASES */
+     app.get("/reviews", async(req, res)=>{
+         const query = {};
+         const cursor = await reviewsCollection.find(query);
+         const result = await cursor.toArray();
+         res.send(result)
+     });     
+
+
+    }catch(err){
+        console.log(err.message);
+    }
+
+
+};
+run().catch(console.dir)
+
+
+
+
 
 
 
