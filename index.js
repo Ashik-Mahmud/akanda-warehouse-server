77ee5d2d7374966077278f57ef60210877f81efa
ashikmahmud <ashikmamud187@gmail.com>
2022-05-01 06:08:48 +0600

    create a add product endpoints and getting product endpoints as well

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 /* init nodejs api */
+var jwt = require('jsonwebtoken');
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require("dotenv").config();
 // init 
 const app = express();
@@ -27,9 +28,9 @@ async function run(){
 
     try{
      await client.connect();
-     console.log("DB CONNECTED.");
      const reviewsCollection =  client.db("reviews-db").collection("reviews");
      const teamsCollection = client.db("teams-db").collection("teams")
+     const productsCollection = client.db("products-db").collection('products');
      /* GET REVIEWS FROM MONGODB DATABASES */
      app.get("/reviews", async(req, res)=>{
          const query = {};
@@ -46,7 +47,67 @@ async function run(){
          res.send({success: true, data: result})
      })
      
-     
+
+
+
+     /* ADD PRODUCT INTO MONGODB SERVER */
+     app.post("/add-product",VerifyToken, async(req,res) => {
+         const data = req.body.data;
+         const decodedId = req.decoded;
+         const sendingUid = req.body.data.author.uid;
+         if(decodedId.uid === sendingUid){
+             const result = await productsCollection.insertOne(data);
+             if(result.acknowledged){
+                 res.send({success: true, message: "Product Added successfully done."})
+             }
+         }else{
+             res.status(403).send({success: false, message: "Forbidden Request"})
+         };
+                     
+     })
+
+     /* GET ALL THE PRODUCTS FROM MONGODB */
+     app.get("/products", async(req, res) => {
+         const query = {};
+         const cursor = await productsCollection.find(query);
+         const result = await cursor.toArray();
+         res.send({success: true, result: result})
+     })
+
+    /*  GET ALL THE PRODUCTS BASED ON CURRENT USER */
+    app.get("/currentUser-product", VerifyToken, async(req, res)=>{
+        const uid = req.decoded.uid;
+        const userUid = req.query.uid;
+                
+        if(uid === userUid){
+
+          const query = {"author.uid": userUid};
+          const cursor = await productsCollection.find(query);
+          const result = await cursor.toArray();
+          res.send({success: true, result: result})
+
+
+        }else{
+            res.status(403).send({success: false, message: "Forbidden Request"})
+        }
+        
+        
+    })
+
+
+
+
+
+     /* GET A USER INFO AND CREATE A ACCESS_TOKEN */
+     app.post("/login", async(req, res) => {
+         const info = req.body;
+         var token = jwt.sign(info, process.env.ACCESS_TOKEN, {
+             expiresIn: '1d'
+         });
+         if(token){
+             res.send({token})
+         };
+     })
 
 
     }catch(err){
@@ -58,7 +119,20 @@ async function run(){
 run().catch(console.dir)
 
 
-
+/* VERIFY TOKEN */
+function VerifyToken(req, res, next){
+    const authToken = req?.body?.authorization?.split(" ")[1] || req?.headers?.authorization?.split(" ")[1];
+    if(!authToken){
+        return res.status(401).send({success: false, message: "Unauthorized Users"})
+    }
+    jwt.verify(authToken, process.env.ACCESS_TOKEN, function(err, decoded) {
+       if(err){
+           return res.status(403).send({success: false, message: "Request Forbidden"})
+       }
+       req.decoded = decoded;
+       next();
+    });    
+};
 
 
 
